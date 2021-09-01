19e7237a0f685e299990cac19f8c9d616fd546b8
MaPan <meetmapan@gmail.com>
2021-09-01 14:16:05 +0800

    add good night && water alter.

---
+++ drink_water.py
@@ -0,0 +1,43 @@
+import os
+import requests
+import json
+import time
+
+def main():
+    send_url = os.environ["send_url"]
+    send_key = os.environ["send_key"]
+
+    is_holiday_url = "http://timor.tech/api/holiday/info/"
+
+    holiday_str = requests.request("GET", is_holiday_url, headers="", data="").text
+
+    holiday_flag = json.loads(holiday_str).get("type").get("type")
+
+    now = time.strftime("%H点%M分", time.localtime())
+
+    title = "[久 坐 提 醒]"
+
+    content = "现在时间" + now + "，摸鱼时间到!  你已经坐下很久了，不要忘记喝水哦。起来走几步，划划水，摸摸鱼，再继续工作吧～"
+
+    msg = title + "\n\n" + content
+
+    data = {
+        "sendkey": send_key,
+        "msg_type": "text",
+        "msg": msg
+    }
+
+    payload = json.dumps(data)
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
+    if holiday_flag == 0 or holiday_flag == 3:
+        response = requests.request("POST", send_url, headers=headers, data=payload)
+        print(response.text)
+    else:
+        print("今天不是工作日，不发送提醒")
+
+
+if __name__ == '__main__':
+    main()
