0c4e45d1ab604a301720933df75c5f18d4ccaeec
mapxn <16299874+mapxn@users.noreply.github.com>
2021-09-23 22:16:47 +0800

    Update drink_water.py

--- drink_water.py
+++ drink_water.py
@@ -3,6 +3,7 @@ import requests
 import json
 from datetime import datetime, timedelta, timezone
 
+
 def main():
     send_url = os.environ["send_url"]
     send_key = os.environ["send_key"]
@@ -13,14 +14,68 @@ def main():
 
     holiday_flag = json.loads(holiday_str).get("type").get("type")
 
-    now = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=8))).strftime("%H点%M分")
+    current_date = datetime.utcnow().replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=8)))
+    now = current_date.strftime("%-m月%-d日")
 
-    title = "[ 久坐提醒 ]"
+    # 下一个节假日时间
+    next_holiday_url = "https://timor.tech/api/holiday/next/?type=N&week=Y"
+    next_holiday_str = requests.request("GET", next_holiday_url, headers="", data="").text
+    next_1_holiday_date_str = json.loads(next_holiday_str).get("holiday").get("date")  # 下一个节假日的日期
+    next_1_holiday_date = datetime.strptime(next_1_holiday_date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=8)))
+    next_1_holiday_name = json.loads(next_holiday_str).get("holiday").get("name").replace("六", "末")  # 下一个节假日名字
+    day_1s = (next_1_holiday_date - current_date).days  # 下一个节假日还有多少天
 
-    content = "现在时间" + now + "，摸鱼时间到!  你已经坐下很久了，不要忘记喝水哦。起来走几步，划划水，摸摸鱼，再继续工作吧～"
+    next_work_url = f"http://timor.tech/api/holiday/workday/next/{next_1_holiday_date_str}"
+    next_work_str = requests.request("GET", next_work_url, headers="", data="").text
+    next_work_data = json.loads(next_work_str).get("workday").get("date")
+
+    # 第2个节假日时间
+    next_holiday_url = f"""https://timor.tech/api/holiday/next/{next_work_data}?type=N&week=N"""
+    next_holiday_str = requests.request("GET", next_holiday_url, headers="", data="").text
+    next_2_holiday_date_str = json.loads(next_holiday_str).get("holiday").get("date")  # 第2个节假日的日期
+    next_2_holiday_date = datetime.strptime(next_2_holiday_date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=8)))
+    next_2_holiday_name = json.loads(next_holiday_str).get("holiday").get("name")  # 第2个节假日名字
+    day_2s = (next_2_holiday_date - current_date).days  # 第2个节假日还有多少天
+
+    next_work_url = f"http://timor.tech/api/holiday/workday/next/{next_2_holiday_date_str}"
+    next_work_str = requests.request("GET", next_work_url, headers="", data="").text
+    next_work_data = json.loads(next_work_str).get("workday").get("date")
+
+    # 第3个节假日时间
+    next_holiday_url = f"""https://timor.tech/api/holiday/next/{next_work_data}?type=N&week=N"""
+    next_holiday_str = requests.request("GET", next_holiday_url, headers="", data="").text
+    next_3_holiday_date_str = json.loads(next_holiday_str).get("holiday").get("date")  # 第3个节假日的日期
+    next_3_holiday_date = datetime.strptime(next_3_holiday_date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=8)))
+    next_3_holiday_name = json.loads(next_holiday_str).get("holiday").get("name")  # 第3个节假日名字
+    day_3s = (next_3_holiday_date - current_date).days  # 第3个节假日还有多少天
+
+    next_work_url = f"http://timor.tech/api/holiday/workday/next/{next_3_holiday_date_str}"
+    next_work_str = requests.request("GET", next_work_url, headers="", data="").text
+    next_work_data = json.loads(next_work_str).get("workday").get("date")
+
+    # 第4个节假日时间
+    next_holiday_url = f"""https://timor.tech/api/holiday/next/{next_work_data}?type=N&week=N"""
+    next_holiday_str = requests.request("GET", next_holiday_url, headers="", data="").text
+    next_4_holiday_date_str = json.loads(next_holiday_str).get("holiday").get("date")  # 第4个节假日的日期
+    next_4_holiday_date = datetime.strptime(next_4_holiday_date_str, "%Y-%m-%d").replace(tzinfo=timezone.utc).astimezone(timezone(timedelta(hours=8)))
+    next_4_holiday_name = json.loads(next_holiday_str).get("holiday").get("name")  # 第4个节假日名字
+    day_4s = (next_4_holiday_date - current_date).days  # 第4个节假日还有多少天
+
+
+    title = "[ 摸鱼提醒 ]"
+
+    content = now + f"""下午好，摸鱼人
+工作再累，一定不要忘记摸鱼哦
+有事没事起身去茶水间去厕所去廊道走走，别老在工位上坐着。
+钱是老板的，但命是自己的。
+距离{next_1_holiday_name}还有{day_1s}天
+距离{next_2_holiday_name}假期还有{day_2s}天
+距离{next_3_holiday_name}假期还有{day_3s}天
+距离{next_4_holiday_name}假期还有{day_4s}天"""
 
     msg = title + "\n\n" + content
-
+    print(msg)
+    
     data = {
         "sendkey": send_key,
         "msg_type": "text",
